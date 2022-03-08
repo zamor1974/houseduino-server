3d8e80cee6a204869049335f2574fd804c0206a4
zamor1974 <luigi.amoroso@gmail.com>
2022-03-08 20:07:01 +0100

    commit

--- src/app.js
+++ src/app.js
@@ -28,63 +28,51 @@ Subscribe (listen) to MQTT topic and start publishing
 simulated data after successful MQTT connection 
 */
 mqttClientTemperature.on('connect', () => {
-    console.log('MqttTemperature connected.')
     mqttClientTemperature.subscribe(mqttTopicTemperature); //subscribe
     //startStreamSimulation();
 })
 mqttClientActivity.on('connect', () => {
-    console.log('MqttActivity connected.')
     mqttClientActivity.subscribe(mqttTopicActivity); //subscribe
     //startStreamSimulation();
 })
 mqttClientAltitude.on('connect', () => {
-    console.log('MqttAltitude connected.')
     mqttClientAltitude.subscribe(mqttTopicAltitude); //subscribe
     //startStreamSimulation();
 })
 mqttClientHumidity.on('connect', () => {
-    console.log('MqttHumidity connected.')
     mqttClientHumidity.subscribe(mqttTopicHumidity); //subscribe
     //startStreamSimulation();
 })
 mqttClientRain.on('connect', () => {
-    console.log('MqttRain connected.')
     mqttClientRain.subscribe(mqttTopicRain); //subscribe
     //startStreamSimulation();
 })
 mqttClientPression.on('connect', () => {
-    console.log('MqttPression connected.')
     mqttClientPression.subscribe(mqttTopicPression); //subscribe
     //startStreamSimulation();
 })
 
 mqttClientTemperature.on('offline', () => {
-    console.log('MqttTemperature offline.')
     mqttClient.unsubscribe(mqttTopicTemperature);
     clearInterval(streamInterval);
 })
 mqttClientActivity.on('offline', () => {
-    console.log('MqttActivity offline.')
     mqttClientActivity.unsubscribe(mqttTopicActivity);
     clearInterval(streamInterval);
 })
 mqttClientAltitude.on('offline', () => {
-    console.log('MqttAltitude offline.')
     mqttClientAltitude.unsubscribe(mqttTopicAltitude);
     clearInterval(streamInterval);
 })
 mqttClientHumidity.on('offline', () => {
-    console.log('MqttHumidity offline.')
     mqttClientHumidity.unsubscribe(mqttTopicHumidity);
     clearInterval(streamInterval);
 })
 mqttClientPression.on('offline', () => {
-    console.log('MqttPression offline.')
     mqttClientPressionunsubscribe(mqttTopicPression);
     clearInterval(streamInterval);
 })
 mqttClientRain.on('offline', () => {
-    console.log('MqttRain offline.')
     mqttClientRain.unsubscribe(mqttTopicRain);
     clearInterval(streamInterval);
 })
@@ -94,7 +82,6 @@ Message event fires, when new messages
 arrive on the subscribed topic
 */
 mqttClientTemperature.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var temperature = 0;
 
     var valore = ['{"temperature":"', message, '"}'].join('');
@@ -102,7 +89,6 @@ mqttClientTemperature.on('message', function (topic, message) {
 })
 
 mqttClientActivity.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var activity = 0;
 
     var valore = ['{"activity":"', message, '"}'].join('');
@@ -110,38 +96,30 @@ mqttClientActivity.on('message', function (topic, message) {
 })
 
 mqttClientHumidity.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var humidity = 0;
 
     var valore = ['{"humidity":"', message, '"}'].join('');
-    let parsedMessage = JSON.parse(message);
     io.emit('humidityData', valore);
 })
 
 mqttClientPression.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var pression = 0;
 
     var valore = ['{"pression":"', message, '"}'].join('');
-    let parsedMessage = JSON.parse(message);
-    io.emit('pressionData', valore);
+   io.emit('pressionData', valore);
 })
 
 mqttClientRain.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var rain = 0;
 
     var valore = ['{"rain":"', message, '"}'].join('');
-    let parsedMessage = JSON.parse(message);
     io.emit('rainData', valore);
 })
 
 mqttClientAltitude.on('message', function (topic, message) {
-    //console.log('Received: ' + message.toString() + ' from topic: ' + topic.toString());
     var altitude = 0;
 
     var valore = ['{"altitude":"', message, '"}'].join('');
-    let parsedMessage = JSON.parse(message);
     io.emit('altitudeData', valore);
 })
 /* 
